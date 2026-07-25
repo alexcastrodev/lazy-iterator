60e47de9169a8af8c0dcfb809035af36862c0298
alexcastrodev <alexoliveira7x@gmail.com>
2026-07-25 12:49:35 +0100

    fix: misunderstanding between iterable with iterator

--- src/SolutionIter.java
+++ src/SolutionIter.java
@@ -4,64 +4,72 @@ import java.io.Reader;
 import java.util.Iterator;
 import java.util.regex.Pattern;
 
-public class SolutionIter implements Iterator<Integer> {
+public class SolutionIter implements Iterable<Integer> {
     private final BufferedReader reader;
-    private Integer nextValue;
-    private boolean loaded = false;
-    private boolean finished = false;
-    private final Pattern pattern = Pattern.compile("[+-]?(0|[1-9][0-9]*)");
 
     public SolutionIter(Reader inp) {
         this.reader = new BufferedReader(inp);
     }
 
+    @Override
     public Iterator<Integer> iterator() {
-        return new SolutionIter(reader);
+        return new SolutionIterIterator();
     }
 
-    @Override
-    public boolean hasNext() {
-        if (finished) {
-            return false;
-        }
+    private class SolutionIterIterator implements Iterator<Integer> {
+        private Integer nextValue;
+        private boolean loaded = false;
+        private boolean finished = false;
+        private final Pattern pattern = Pattern.compile("[+-]?(0|[1-9][0-9]*)");
 
-        if (loaded) {
-            return true;
-        }
+        @Override
+        public boolean hasNext() {
+            if (finished) {
+                return false;
+            }
 
-        try {
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                line = line.trim();
-
-                if (!pattern.matcher(line).matches()) {
-                    continue;
-                }
-
-                // A valid integer is a sequence of one or more digits (without leading zeros),
-                // optionally preceded by a plus or minus sign, representing a number within the range
-                // [-1,000,000,000.1,000,000,000].
-                long value = Long.parseLong(line);
-                if (value < -1000000000L ||  value > 1000000000L) {
-                    continue;
-                }
-
-                nextValue = (int) value;
-                loaded = true;
+            if (loaded) {
                 return true;
             }
 
-            finished = true;
-            return false;
-        } catch (IOException e) {
-            finished = true;
-            return false;
-        }
-    }
+            try {
+                String line;
 
-    @Override
-    public Integer next() {
-        return 0;
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+
+                    if (!pattern.matcher(line).matches()) {
+                        continue;
+                    }
+
+                    // A valid integer is a sequence of one or more digits (without leading zeros),
+                    // optionally preceded by a plus or minus sign, representing a number within the range
+                    // [-1,000,000,000.1,000,000,000].
+                    long value = Long.parseLong(line);
+                    if (value < -1000000000L ||  value > 1000000000L) {
+                        continue;
+                    }
+
+                    nextValue = (int) value;
+                    loaded = true;
+                    return true;
+                }
+
+                finished = true;
+                return false;
+            } catch (IOException e) {
+                finished = true;
+                return false;
+            }
+        }
+
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+            loaded = false;
+            return nextValue;
+        }
     }
 }
