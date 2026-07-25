9a6f7a0328afb8acb76e2da65f5fd32a5351e4bd
alexcastrodev <alexoliveira7x@gmail.com>
2026-07-25 12:29:09 +0100

    implement the readline with regex

--- src/SolutionIter.java
+++ src/SolutionIter.java
@@ -1,9 +1,15 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.regex.Pattern;
 
 public class SolutionIter implements Iterator<Integer> {
     private final BufferedReader reader;
+    private Integer nextValue;
+    private boolean loaded = false;
+    private boolean finished = false;
+    private final Pattern pattern = Pattern.compile("[+-]?(0|[1-9][0-9]*)");
 
     public SolutionIter(Reader inp) {
         this.reader = new BufferedReader(inp);
@@ -15,7 +21,28 @@ public class SolutionIter implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        return false;
+        if (finished) {
+            return false;
+        }
+
+        if (loaded) {
+            return true;
+        }
+
+        try {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                line = line.trim();
+
+                if (!pattern.matcher(line).matches()) {
+                    continue;
+                }
+            }
+        } catch (IOException e) {
+            finished = true;
+            return false;
+        }
     }
 
     @Override
