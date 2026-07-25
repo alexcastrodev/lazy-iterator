75cb3fcfdfb3afa3dfc7a8b29823084e0772c08c
alexcastrodev <alexoliveira7x@gmail.com>
2026-07-25 12:20:13 +0100

    implements interface

--- src/SolutionIter.java
+++ src/SolutionIter.java
@@ -1,8 +1,22 @@
 import java.io.Reader;
 import java.util.Iterator;
 
-public class SolutionIter {
-    public SolutionIter(Reader inp);
+public class SolutionIter implements Iterator<Integer> {
+    public SolutionIter(Reader inp) {
 
-    public Iterator<Integer> iterator();
+    }
+
+    public Iterator<Integer> iterator() {
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        return 0;
+    }
 }
